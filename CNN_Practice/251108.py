fa42f5c0a44372f49c56b0d98b0b25adf29e365b
abbeyroad1027-glitch <abbeyroad1027@gmail.com>
2025-11-08 00:48:54 +0900

    CNN Partial

--- CNN_Practice/251108.py
+++ CNN_Practice/251108.py
@@ -33,3 +33,29 @@ p_out = pool1(r_out)
 print(f"\nOriginal shape: {dummy_image.shape}")
 print(f"After Conv1: {c_out.shape}")
 print(f"After MaxPool1: {p_out.shape}")
+
+print("=======================================================")
+
+class PartialCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(10*12*12, 50)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(50,10)
+
+    def forward(self, x_pool2):
+        x_flat = x_pool2.view(x_pool2.size(0),-1)
+
+        x = self.fc1(x_flat)
+        x = self.relu(x)
+        x = self.fc2(x)
+        return x, x_flat
+    
+net_cnn = PartialCNN()
+dummy_pool_out = torch.randn(100, 10, 12, 12)
+final_output, flat_output = net_cnn(dummy_pool_out)
+
+print(f"Model : \n{net_cnn}")
+print(f"\nShape before Flatten: {dummy_pool_out.shape}")
+print(f"\nShape after Flatten: {flat_output.shape}")
+print(f"Final Output Shape: {final_output.shape}")
