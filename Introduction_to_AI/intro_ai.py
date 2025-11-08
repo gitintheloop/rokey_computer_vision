a3f9b6673cef6aa8d430098feb66270d7cfcccf3
abbeyroad1027-glitch <abbeyroad1027@gmail.com>
2025-11-08 23:31:30 +0900

    intro

--- Introduction_to_AI/intro_ai.py
+++ Introduction_to_AI/intro_ai.py
@@ -34,3 +34,27 @@ with torch.no_grad():
 
 W.grad.zero_()
 B.grad.zero_()
+
+print("="*70)
+
+print(f"W after Update: {W.item():.4f}")
+print(f"B after Update: {B.item():.4f}")
+print(f"W current gradient: {W.grad}")
+print(f"B current gradient: {B.grad}")
+
+X = torch.tensor([-5., 5., 0., 2., -2.]).float()
+Y = torch.tensor([-6.7, 10.3, -3.3, 5.0, -5.3]).float()
+
+W = torch.tensor(1.0, requires_grad=True).float()
+B = torch.tensor(1.0, requires_grad=True).float()
+lr = 0.001
+
+def pred(x): return W * X + B
+def mse(Yp, Y): return ((Yp - Y)**2).mean()
+
+optimizer = optim.SGD([W,B],lr)
+Yp = pred(X)
+loss = mse(Yp, Y)
+loss.backward()
+optimizer.step()
+optimizer.zero_grad()
